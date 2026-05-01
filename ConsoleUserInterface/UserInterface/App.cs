9f944b799c3e527cbbc92814863e47e7a6e9a758
bmckay18 <106320005+bmckay18@users.noreply.github.com>
2026-05-01 19:07:53 +1000

    Update App.cs

--- ConsoleUserInterface/UserInterface/App.cs
+++ ConsoleUserInterface/UserInterface/App.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Service.Transactions;
+using Service.Transactions.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,12 @@ namespace ConsoleUserInterface.UserInterface
 {
     public class App
     {
+        private ITransactionsService _service;
+
+        public App(ITransactionsService service)
+        {
+            _service = service;
+        }
         public void Run()
         {
             Console.ReadKey();
